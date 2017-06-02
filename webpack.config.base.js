9597c8a367110995466b58f72a61a4e0b42795e3
Alexander Rath <alex@localhost.local>
2017-06-02 18:42:20 +0200

    Enhance hot reloading for TypeScript files (#1)

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,7 +12,7 @@ module.exports = validate({
   module: {
     loaders: [{
       test: /\.tsx?$/,
-      loaders: ['ts-loader'],
+      loaders: ['react-hot-loader', 'ts-loader'],
       exclude: /node_modules/
     }, {
       test: /\.json$/,
